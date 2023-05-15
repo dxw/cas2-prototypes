191055274db1edc22cc833c24a8516836bd63546
Alexandra Edwards <alex.edwards@WQW254QMXJ.local>
2023-05-15 16:46:05 +0100

    Risk checkbox prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,21 +5,48 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
 
-  // Risk section - Switch text area edit state based on radio selection
-  $('#radio-edited').on('click', (function(){
-    $('#oasys-textarea').attr("disabled", false);
-    $('#button-save-and-continue').addClass("govuk-button--disabled");
+  var editText = $('#edit-text');
+  var revertText = $('#revert-text');
+  var upToDate = $('#up-to-date');
+  var textArea = $('#oasys-textarea');
+  var continueButton = $('#button-save-and-continue');
+  
+  var originalOASys = $('#oasys-textarea').text();
+  
+  // Risk section - Click "edit text", make text area editable, hide 'edit text' button, show 'revert to oasys' button 
+  $(editText).on('click', (function(){
+    $(textArea).attr("disabled", false);
+    $(continueButton).addClass("govuk-button--disabled");
+    $(revertText).removeClass('govuk-!-display-none');
+    $(this).addClass('govuk-!-display-none');
+
+    if ((upToDate).prop("checked") === true) {
+      upToDate.prop("checked", false);
+    } else {
+      console.log('not checked');
+    }
+
   }))
 
-  $('#radio-up-to-date').on('click', (function(){
-    $('#oasys-textarea').attr("disabled", true);
+  // Risk section - Check 'confirm up to date' checkbox, disable text area
+  $(upToDate).on('click', (function(){
+    $(textArea).attr("disabled", true);
+    $(continueButton).attr("disabled", false);
+    $(continueButton).removeClass("govuk-button--disabled");
+    $(editText).removeClass('govuk-!-display-none');
+    $(revertText).addClass('govuk-!-display-none');
   }))
 
-  // Risk section - Change click status and styling of confirm button based on above selection
-  $('#radio-up-to-date').on('click', (function(){
-    $('#button-save-and-continue').attr("disabled", false);
-    $('#button-save-and-continue').removeClass("govuk-button--disabled");
+  // Risk section - Click 'revert back to oasys', replace whatever is in text area with original value
+  $(revertText).on('click', (function(){
+    $(textArea).val(originalOASys);
   }))
+
+  if ((upToDate).prop("checked") === true) {
+    upToDate.prop("checked", false);
+  } else {
+    console.log('not checked');
+  }
 
 })
 
